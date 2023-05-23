8d1857f4d9ac01caa069d12689ef5bf91e688361
nada ali <106203694+Nadaali18@users.noreply.github.com>
2023-05-23 16:36:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class BusnissCardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 97, 45, 91),
         body: Column(
